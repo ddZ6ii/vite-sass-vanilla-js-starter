cd625a06024ed5bb883660d016422ce9d2c64771
Denis Dezest <denis.dezest@gmail.com>
2024-04-07 18:42:01 +0200

    📦 Update optimization plugins
    Details:
      - remove `autoprefixer`: Vite already add vendor prefixes
      - remove `postcss-uncss`: replaced by `vite-plugin-purgecss-updated-v5`
      - add `vite-plugin-minify` for html minification
      - replace .prettierrc config file by prettier.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,7 @@
 import { defineConfig } from 'vite';
+import { ViteMinifyPlugin } from 'vite-plugin-minify';
+import pluginPurgeCss from 'vite-plugin-purgecss-updated-v5';
+
 
 // Define chrome as default browser for the dev server.
 const opsys = process.platform;
@@ -15,4 +18,17 @@ export default defineConfig({
     emptyOutDir: true,
     sourcemap: true,
   },
+    plugins: [
+    // HTML minification
+    ViteMinifyPlugin({
+      removeComments: true,
+    }),
+    // Purge CSS: remove unused CSS class
+    pluginPurgeCss({
+      content: ['./src/**/*.html'],
+      sourceMap: true,
+      variables: true,
+    }),
+  ],
 });
+
